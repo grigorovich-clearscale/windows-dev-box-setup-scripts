bd3fc6f0130ac46bb9c4d8662817bb779fa17e6e
Alexey Grigorovich <alexey.grigorovich@clearscale.net>
2018-12-30 14:02:10 +0700

    git: fix parameter escaping

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,7 @@
 
 # tools we expect devs across many scenarios will want
 cinst -y vscode
-cinst -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+cinst -y git --package-parameters "'/GitAndUnixToolsOnPath /WindowsTerminal'"
 cinst -y python
 cinst -y 7zip.install
 cinst -y sysinternals
